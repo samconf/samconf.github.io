7b31e07ea64d02effa762e658bbc5de667d84a4d
Kunal Kamath <kkamath101@gmail.com>
2016-07-14 01:11:33 -0400

    Set up proper header and footer

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,6 +1,26 @@
-$(document).on("scroll", function(){
-    if($(document).scrollTop() > 100)
-      $("header").addClass("shrink");
-    else
-        $("header").removeClass("shrink");
+$(document).ready(function() {
+    if ($('#endpoint-index').length === 0) {
+        return;
+    }
+
+    $(document).on("scroll", function(){
+        if($(document).scrollTop() > 100)
+          $("header").addClass("shrink");
+        else
+            $("header").removeClass("shrink");
+    });
+
+    $('a').click(function(evt) {
+    var $target = $(evt.target.hash);
+
+    if ($target.length === 1) {
+        $('html, body').animate({
+            scrollTop: $target.offset().top
+        }, 500);
+
+        evt.preventDefault();
+        evt.stopImmediatePropagation();
+    }
+});
+
 });
