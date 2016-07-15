a1781deecd763b6483a9517eed23b3077a6c0c90
Kunal Kamath <kkamath101@gmail.com>
2016-07-15 01:15:21 -0400

    Site basics implemented, although header refactoring is needed

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,26 +1,47 @@
 $(document).ready(function() {
-    if ($('#endpoint-index').length === 0) {
-        return;
+
+    if ($('#endpoint-index').length != 0) {
+        $("header").addClass("index-header");
     }
 
     $(document).on("scroll", function(){
-        if($(document).scrollTop() > 100)
-          $("header").addClass("shrink");
-        else
-            $("header").removeClass("shrink");
+        if($(document).scrollTop() > 50) {
+            $("header").attr("id", "shrink");
+            if ($('#endpoint-index').length != 0) {
+                $("#about-bar").parent().addClass("active-link");
+            }
+        }
+        else {
+            $("header").removeAttr("id", "shrink");
+            $("#about-bar").parent().removeClass("active-link");
+        }
+    });
+
+    $('#navbar a').each(function() {
+        if ($(this).attr('href') === window.location.pathname) {
+            $(this).parent().addClass('active-link');
+        }
+        else {
+            $(this).parent().removeClass('active-link');
+        }
     });
 
     $('a').click(function(evt) {
-    var $target = $(evt.target.hash);
+        var $target = $(evt.target.hash);
+            if ($target.length === 1) {
+                if($(document).scrollTop() < 50 && $('#endpoint-index').length != 0) {
+                    $('html, body').animate({
+                        scrollTop: $target.offset().top
+                    }, 500);
+                }
+                else {
+                    $('html, body').animate({
+                        scrollTop: $target.offset().top+38
+                    }, 500);
+                }
 
-    if ($target.length === 1) {
-        $('html, body').animate({
-            scrollTop: $target.offset().top
-        }, 500);
-
-        evt.preventDefault();
-        evt.stopImmediatePropagation();
-    }
-});
-
+                evt.preventDefault();
+                evt.stopImmediatePropagation();
+            }
+    });
 });
